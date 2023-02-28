64b580811071dc16c28a1971280be335bdbdbfe0
Patricio Villalobos <patricio@glacier.fi>
2023-02-28 17:34:17 +0000

    use opam to install deps, clp for file preview in fzf

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "Dev env for ocaml";
 
   inputs = {
      fzf_tmux_kak = {
        url = "github:jpe90/fzf-tmux.kak";
        flake = false;
      };
+
+     clp_src= {
+       url = "github:jpe90/clp";
+       flake = false;
+     };
+
   };
 
-  outputs = { self, fzf_tmux_kak, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    outputs = { self, clp_src, fzf_tmux_kak, nixpkgs }:
+    let
+    	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    	clp = pkgs.llvmPackages_15.libcxxStdenv.mkDerivation
+    	{
+          name = "clp";
+          src = clp_src;
+          buildInputs = [ pkgs.pkg-config pkgs.luajit pkgs.luajitPackages.lpeg pkgs.luajitPackages.luautf8 ];
+    	};
+
     in {
       devShells.x86_64-linux.default =
         pkgs.mkShell.override {stdenv = pkgs.llvmPackages_15.libcxxStdenv;}
@@ -20,32 +35,32 @@
           kakrc = ./config/kak;
           kak_lsp_cfg = ./config/kak-lsp/kak-lsp.toml;
           kak_fzf = fzf_tmux_kak;
+          clp = clp;
 
           shellHook = ''
               export ENV_NAME="kak"
               export KAKOUNE_CONFIG_DIR="$kakrc"
               # export OCAMLPARAM="verbose=1,_"
-              export PATH=$PATH:$gccwrap
+              export PATH=$PATH:$clp:$gccwrap
               export EDITOR=kak
+              eval $(opam env)
           '';
+
+	  inputsFrom = [ clp ];
 
           buildInputs = [
               pkgs.zsh
 
               pkgs.fzf
-              pkgs.clp
               pkgs.fd
 
               pkgs.kakoune
               pkgs.kak-lsp
               pkgs.nil
-              pkgs.ocaml
-              pkgs.opam
-              pkgs.ocamlformat
 
-              pkgs.ocamlPackages.ocaml-lsp
-              pkgs.ocamlPackages.dune_3
-              pkgs.ocamlPackages.utop
+              pkgs.opam
+
+              clp
           ];
         };
     };
